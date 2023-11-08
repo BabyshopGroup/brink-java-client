4611a05c379954b8b3b1fb7ff246ddd68fca846f
MarcusLindbloom <marcus.lindblom@me.com>
2023-11-08 12:42:48 +0100

    Update BrinkVariantInventory

--- src/main/java/com/brinkcommerce/api/management/stock/productvariants/BrinkVariantInventory.java
+++ src/main/java/com/brinkcommerce/api/management/stock/productvariants/BrinkVariantInventory.java
@@ -1,8 +1,13 @@
 package com.brinkcommerce.api.management.stock.productvariants;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.Objects;
 
-public record BrinkVariantInventory(Integer quantity, String variantId, String inventoryId) {
+public record BrinkVariantInventory(
+    Integer quantity,
+    @JsonProperty("productVariantId") String variantId,
+    String inventoryId) {
   public BrinkVariantInventory {
     Objects.requireNonNull(quantity, "A Variant Inventory can not have quantity = null");
     Objects.requireNonNull(variantId, "A Variant Inventory can not have variantId = null");
